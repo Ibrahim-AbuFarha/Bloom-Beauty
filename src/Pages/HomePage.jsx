653e4bf34ff0bccadf2bad73909be48b8f607f09
bayanghaben <bayanghabin@gmail.com>
2023-06-22 12:28:08 +0300

    a

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,13 +1,12 @@
 // collection of your components page
 import CarouselWrapper from "../components/HomePage/CarouselWrapper";
 import BestSellerWrapper from "../components/HomePage/BestSellerWrapper";
-import Testimonials from "../components/HomePage/Testimomials";
+
 export default function HomePage() {
   return (
     <>
       <CarouselWrapper />
       <BestSellerWrapper />
-      <Testimonials />
     </>
   );
 }
