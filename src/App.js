a22ea52e7fc480cca95460e0ec33109d53808f5c
AmerANaser <amer.naasser@gmail.com>
2023-06-22 15:14:05 +0300

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // import ProductPage from "./Pages/ProductPage";
 // import CartPage from "./Pages/CartPage";
-// import SignUp from "./components/SignUp/SignUp";
+import SignUp from "./components/SignUp/SignUp";
 // import SignIn from "./components/SingIp/SignIn";
 
 // axios.post('localhost:3001/users',{name , password , email , })
@@ -8,7 +8,7 @@
 function App() {
   return (
     <>
-
+   <SignUp/>
     </>
   );
 
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -6,6 +6,12 @@ function SignUp() {
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
   const [email, setEmail] = useState();
+  const [address , setAddress]=useState();
+  const [country, setCountry] = useState();
+  const [phone, setPhone] = useState();
+  const [postalCode ,SetPostalCode]=useState();
+  const [city, setCity] = useState();
+
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -14,6 +20,11 @@ function SignUp() {
         username,
         password,
         email,
+        city,
+        postalCode,
+        phone,
+        country,
+        address,
       });
 
       await axios.post("http://localhost:3001/carts", {
@@ -33,13 +44,53 @@ function SignUp() {
         type="text"
         onChange={(e) => setUserName(e.target.value)}
         required
+        placeholder="username"
       />
-      <input type="text" onChange={(e) => setEmail(e.target.value)} required />
       <input
-        type="text"
+        type="email"
+        onChange={(e) => setEmail(e.target.value)}
+        required
+        placeholder="Email"
+      />
+      <input
+        type="password"
         onChange={(e) => setPassword(e.target.value)}
         required
+        placeholder="Password"
       />
+
+      <input
+        type="text"
+        onChange={(e) => setCountry(e.target.value)}
+        required
+        placeholder="Country"
+      />
+      <input
+        type="text"
+        onChange={(e) => setAddress(e.target.value)}
+        required
+        placeholder="Address"
+      />
+      <input
+        type="text"
+        onChange={(e) => setCity(e.target.value)}
+        required
+        placeholder="City"
+      />
+
+      <input
+        type="text"
+        onChange={(e) => SetPostalCode(e.target.value)}
+        required
+        placeholder=" Postal Code"
+      />
+      <input
+        type="text"
+        onChange={(e) => setPhone(e.target.value)}
+        required
+        placeholder="Phone"
+      />
+
       <button>Submit</button>
     </form>
   );
