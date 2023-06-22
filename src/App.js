c57f4ed9d8c1e318efecdcbcb081ebaf47d455ed
MohammadAwad97 <mohamad.s.awad97@gmail.com>
2023-06-22 12:11:14 +0300

    delete the double product file

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 // import './App.css';
 
 // import './App.css';
-import Footer from "./components/Footer/Footer";
-import ProfileSection from "./Pages/ProfileSection";
-import ProductsPage from "./components/ProductsPage/ProductsPage";
+// import Footer from "./components/Footer/Footer";
+// import ProfileSection from "./Pages/ProfileSection";
+// import ProductsPage from "./components/ProductsPage/ProductsPage";
 import HomePage from "./components/HomePage/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ProductsPage from "./Pages/ProductsPage"
 
-import SignIn from "./components/SingIp/SignIn";
-import SignUp from "./components/SignUp/SignUp";
+// import SignIn from "./components/SingIp/SignIn";
+// import SignUp from "./components/SignUp/SignUp";
 
 // axios.post('localhost:3001/users',{name , password , email , })
 
 function App() {
   return (
     <div>
+      <ProductsPage />
       <HomePage />
     </div>
   );
