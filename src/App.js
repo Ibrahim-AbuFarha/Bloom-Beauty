c0b281f9994c044979bbd2a3f528b4203dc47c73
[Abdulraof-Mattur] <[abdulraof.y.mattur@gmail.com]>
2023-06-23 15:27:40 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import ProductPage from "./pages/ProductPage";
-import CartPage from "./pages/CartPage";
-import { useEffect } from "react";
-import axios from "axios";
+import ProductPage from "./Pages/ProductPage";
+import CartPage from "./Pages/CartPage";
+
 import SignUp from "./components/SignUp/SignUp";
 
 // axios.post('localhost:3001/users',{name , password , email , })
 
 function App() {
-  return <SignUp></SignUp>;
+  return <CartPage />;
 }
 
 export default App;
