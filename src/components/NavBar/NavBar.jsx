941e5ac81b79e26eade1a5e091c4ab9afab5bc82
Ibrahim-AbuFarha <ibrahim.j.abufarha@gmail.com>
2023-06-24 16:29:32 +0300

    take nav and and products

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,20 @@
 import React, { useContext } from "react";
 import CartContext from "../../store/cartContext";
 import { Link } from "react-router-dom";
-//
 import "../../index.css";
 function NavBar() {
+  const { totalProducts, deleteAllItems } = useContext(CartContext);
+  let isUser = true;
+  const handleLogOut = () => {
+    localStorage.removeItem("user");
+    // deleteAllItems();
+    isUser = false;
+  };
   return (
-    <nav class="navbar  navbar-dark bg-dark  navbar-expand-lg ">
-      <div class="container ">
+    <nav className="navbar  navbar-dark bg-dark  navbar-expand-lg ">
+      <div className="container ">
         <button
-          class="navbar-toggler text-light"
+          className="navbar-toggler text-light"
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarTogglerDemo03"
@@ -16,32 +22,32 @@ function NavBar() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <i class="fa-sharp fa-solid fa-bars" style={{ color: "#ffffff" }}></i>{" "}
+          <i className="fa-sharp fa-solid fa-bars" style={{ color: "#ffffff" }}></i>{" "}
         </button>
-        <a class="navbar-brand logo" href="#">
+        <a className="navbar-brand logo" href="#">
           Bloom Beauty
         </a>
-        <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
                 Shop
               </a>
             </li>
           </ul>
 
-          <div className="d-flex align-items-center gap-3 justify-content-center">
-            <i class="fa-solid fa-circle-user " style={{ color: "#fff" }}></i>
+          <div classNameName="d-flex align-items-center gap-3 justify-content-center">
+            <i className="fa-solid fa-circle-user " style={{ color: "#fff" }}></i>
             <i
-              class="fa-solid cart fa-cart-shopping"
+              className="fa-solid cart fa-cart-shopping"
               style={{ color: "#ffffff" }}
             ></i>
-            <button class="btn fw-bold bg-light text-dark" type="button">
+            <button className="btn fw-bold bg-light text-dark" type="button">
               Logout
             </button>
           </div>
