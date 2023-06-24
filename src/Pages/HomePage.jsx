c514e2fba40f9130f8ca5957835942c1ea314746
bayanghaben <bayanghabin@gmail.com>
2023-06-24 04:13:50 +0300

    edit on landing

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,13 +4,16 @@ import CarouselWrapper from "../components/HomePage/CarouselWrapper";
 import BestSellerWrapper from "../components/HomePage/BestSellerWrapper";
 import YoutubeSection from "../components/HomePage/Youtube";
 import Testimonials from "../components/HomePage/Testimonials.js";
+import CardsDark from "../components/HomePage/CardsDark";
 export default function HomePage() {
   // const apiKey = "AIzaSyCpBrKkjRQ9mk8TpnPjFN5q4WLdNVREFBQ";
   const videoId = "nWTHaG0Wpfg";
   return (
     <>
       <CarouselWrapper />
-      <BestSellerWrapper />
+
+      {/* <BestSellerWrapper /> */}
+      <CardsDark />
       <YoutubeSection videoId={videoId} />
       <Testimonials />
     </>
